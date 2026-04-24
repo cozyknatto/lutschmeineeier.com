6e3635f94e052093770e56ef51c864310a5f62c3
cozy <cozyknatto@gmail.com>
2026-04-24 22:40:50 +0200

    joa

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
-  name: 'Relaxo',
-  bio:  'doing stupid shit on the internet',
+  name: 'Cozy',
+  bio:  'I am a idot',
   avatar: 'img/pfp.png',
 
   discord: '1180856619769274470',
@@ -8,17 +8,17 @@ var CONFIG = {
   hideBadges: ['quest_completed', 'orb_profile_badge'],
 
   socials: [
-    { title: 'GitHub',    url: 'https://github.com/cozyknatto',       icon: 'github'    },
-    { title: 'Steam',    url: 'https://steamcommunity.com/id/muckelig',       icon: 'steam'    },
-    { title: 'Discord Server',   url: 'https://discord.awall.wtf',           icon: 'discord'   },
-    { title: 'Instagram', url: 'https://instagram.com/rlx0.official', icon: 'instagram' },
-    { title: 'Twitter',   url: 'https://x.com/itsrelaxo',            icon: 'twitter'   },
-    { title: 'YouTube',   url: 'https://youtube.com/@itsrelaxo',      icon: 'youtube'   },
-    { title: 'Twitch',    url: 'https://twitch.tv/itsrelaxo',         icon: 'twitch'    },
+    { title: 'GitHub',    url: 'https://github.com/cozyknatto', icon: 'github'    },
+    { title: 'Steam',    url: 'https://steamcommunity.com/id/muckelig', icon: 'steam'    },
+    { title: 'Discord Server',   url: 'https://discord.gg/pYFEHQw22c', icon: 'discord'   },
+    { title: 'Instagram', url: 'https://instagram.com/cozyknatto', icon: 'instagram' },
+    { title: 'Twitter',   url: 'https://x.com/cozyknatto', icon: 'twitter'   },
+    { title: 'YouTube',   url: 'https://youtube.com/@cozyknatto', icon: 'youtube'   },
+    { title: 'Twitch',    url: 'https://twitch.tv/huggcozy', icon: 'twitch'    },
   ],
 
   projects: [
-    {
+    /* {
       name:     'HarderCalender',
       desc:     'Event calendar for the harderstyles scene - Hardstyle, Rawstyle & more. App coming soon!',
       url:      'https://hardercalender.de',
@@ -27,14 +27,14 @@ var CONFIG = {
       img:      'img/hardercalender.png',
       tags:     ['HTML', 'CSS', 'JS', 'NextJS'],
       newTab:   true,
-    },
-    {
+    }, */
+/*     {
       name:     'relaxo.gay',
       desc:     'This page. Personal bio & link hub.',
       url:      'https://relaxo.gay',
       emoji:    '🌐',
       gradient: 'linear-gradient(135deg, #0d2018, #1a3d2b)',
       tags:     ['HTML', 'CSS', 'JavaScript'],
-    },
+    }, */
   ],
 };
